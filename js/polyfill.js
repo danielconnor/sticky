c2df9921e49401cf49675054e21f4068c7c02a2a
danielconnor <danielconnr@gmail.com>
2012-07-22 01:02:23 +0100

    fix tab spacing

--- js/polyfill.js
+++ js/polyfill.js
@@ -3,47 +3,48 @@ window.DOMTokenList = window.DOMTokenList || (function() {
     "use strict";
 
     function DOMTokenList(element) {
-        Array.call(this);
-        this.element = element;
+      Array.call(this);
+      this.element = element;
     }
 
     util.inherits(DOMTokenList, Array);
 
     DOMTokenList.prototype.toString = function() {
-        return this.join(" ");
+      return this.join(" ");
     };
     DOMTokenList.prototype.add = function(className) {
-        if(!this.contains(className)) {
-            this.push(className);
-            this.element.className = this.toString();
-        }
+      if(!this.contains(className)) {
+        this.push(className);
+        this.element.className = this.toString();
+      }
     };
     DOMTokenList.prototype.remove = function(className) {
-        var loc;
-        if((loc = this.indexOf(className)) > -1) {
-            this.splice(loc, 1);
-            this.element.className = this.toString();
-        }
+      var loc;
+      if((loc = this.indexOf(className)) > -1) {
+        this.splice(loc, 1);
+        this.element.className = this.toString();
+      }
     };
     DOMTokenList.prototype.contains = function(className) {
-        return this.indexOf(className) > -1;
+      return this.indexOf(className) > -1;
     };
     DOMTokenList.prototype.item = function(loc) {
-        return this[loc];
+      return this[loc];
     };
     DOMTokenList.prototype.toggle = function(className) {
-        var loc = this.indexOf(className),
-            contains = loc > -1;
+      var loc = this.indexOf(className),
+          contains = loc > -1;
 
-        if(contains) {
-            this.splice(has, className);
-        }
-        else {
-            this.push(className);
-        }
-        this.element.className = this.toString();
+      if(contains) {
+        this.splice(loc, 1);
+      }
+      else {
+        this.push(className);
+      }
+      this.element.className = this.toString();
 
-        return !contains;
+      return !contains;
     };
+    
     return DOMTokenList;
 })();
