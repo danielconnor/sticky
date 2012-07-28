3c4b49f37a12ecf9f39138e226100d11e8d0e413
danielconnor <danielconnr@gmail.com>
2012-07-28 16:43:24 +0100

    add resources panel

---
+++ js/UI.Resources.js
@@ -0,0 +1,20 @@
+/*global util, UI*/
+UI.Resources = (function() {
+  "use strict";
+
+  function Resources(tagName, classes) {
+    UI.Popout.call(this);
+    this.dropzone = new UI.DropZone();
+
+    this.append(this.dropzone);
+
+  }
+  util.inherits(Resources, UI.Popout);
+
+  var _proto = Resources.prototype,
+    _super = UI.Popout.prototype;
+
+
+
+  return Resources;
+})();
